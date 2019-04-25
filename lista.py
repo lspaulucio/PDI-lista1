81b7eb920002a69bb58a7d0fba3550f8a1260689
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2019-04-25 08:59:02 -0300

    Implementing ex2

--- lista.py
+++ lista.py
@@ -10,10 +10,8 @@ kernel2 = np.array([[1, 1, 1],
                     [1, 1, 1],
                     [1, 1, 1]])
 
-print(kernel*kernel2)
+# print(kernel*kernel2)
 
-# def conv2D(img, kernel):
-# print(np.flip(kernel))
 
 def getSubImg(img, i, j):
 
@@ -21,5 +19,36 @@ def getSubImg(img, i, j):
               [img[i][j-1],   img[i][j],   img[i][j+1]],
               [img[i+1][j-1], img[i+1][j], img[i+1][j+1]]])
 
-for i in range(0, shape[0]):
-    for j in range(0, shape[1]):
+
+def getOutputSize(old_shape,kernel_shape, padding, stride):
+    return int((old_shape + 2*padding - kernel_shape)/stride + 1)
+
+def getNewSize(shape, padding):
+    return int((shape + 2*padding))
+
+
+def conv2D(img, kernel, stride=1, padding=1, padding_value=0):
+    shape = []
+    newImg = []
+    for i in range(0, 2):
+        size = getNewSize(img.shape[i], padding)
+        shape.append(size)
+
+    kernel = np.flip(kernel)
+
+    if padding_value == 0:
+        newImg = np.zeros((shape))
+    elif padding_value == 1:
+        newImg = np.ones((shape))
+
+    newImg[padding:-padding, padding:-padding] = img
+
+    
+
+
+
+img = Image.open('images/lena.tif')
+img.show()
+img = np.array(img)
+# print(img.shape)
+conv2D(img, kernel, padding=1)
