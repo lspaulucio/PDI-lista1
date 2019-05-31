60290e3bca08981326018a3edb887cdbf4f64bc4
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2019-05-31 11:44:48 -0300

    Implementing adaptative median

--- ex8.py
+++ ex8.py
@@ -11,6 +11,36 @@ import numpy as np
 import MyLib as ml
 import copy
 
+
+def adaptativeMedian(img, Smax):
+    window_size = (3, 3)
+
+    # zmin valor minimo de intensidade na janela
+    # zmax valor maximo de intensidade na janela
+    # zmed mediana de intensidade na janela
+    # zxy valor do pixel
+    # smax valor maximo da janela
+
+    # zmed = mediana dos valores de intensidade em Sxy
+    # stepA
+    # a1 = zmed -zmin
+    # a2 = zmed - zmax
+    # se a1 > 0 e a2 < 0 goto B
+    # senao aumente sxy
+    # se window_size <= smax repita A
+    # senao saida é zmed
+
+    # stepB
+    # b1 = zxy - zmin
+    # b2 = zxy -zmax
+    # se b1 > 0 e b2 <0 saida é zxy
+    # senao a saida é zmed
+
+    print(window_size)
+
+
+
+
 img = Image.open('images/ruidosa2.tif')
 img = np.array(img)
 imgOld = copy.deepcopy(img)
